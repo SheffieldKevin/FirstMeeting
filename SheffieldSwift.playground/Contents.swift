cc040b47ebbeb0b69d1c20133f51828e6c15fdf5
Kevin Meaney <ktam@yvs.eu.com>
2015-05-12 13:45:11 +0100

    Added discussion about optionals.

--- SheffieldSwift.playground/Contents.swift
+++ SheffieldSwift.playground/Contents.swift
@@ -29,7 +29,7 @@ let newString:String = "Hello lovely people"
 //: #### The types of str and newString are the same
 println("newString type is: \(newString.dynamicType)")
 
-//: #### Type inference works for integers to
+//: #### Type inference works for any type the compiler can reasonably determine at compile time.
 let myNum = 2
 println("\(myNum.dynamicType)")
 
@@ -44,7 +44,7 @@ println(newString)
 
 //: So newString is a constant. newString is also a struct instance, not an object instance and I'll explain about why that is important later on.
 
-//: A variable or a constant does not have to have an initial value as long as the variable is assigned a value before it is accessed. In this case though the decleration requires the type to be specified. The compiler will not attempt to look ahead to infer the type from later information.
+//: A variable or a constant does not have to have an initial value as long as the property is assigned a value before it is accessed. In this case though the decleration requires the type to be specified. The compiler will not attempt to look ahead to infer the type from later information.
 
 // let anotherString
 let theString:String
@@ -116,7 +116,139 @@ myCity.printCity()
 
 //: The word 'instance' is a more general term used to refer to objects of classes or structs.
 
-//: ### Optionals - An important & painful at first Swift feature
-//: If a property can't be assigned when declared or initialized with an undefined value then its value will be either undefined or nil. Swift formalizes what will happen with optionals.
+//: ### Optionals - An important feature of Swift but initially painful
+//: If a property can't be assigned when declared then its value will be either undefined or nil. Some APIs return an Optional value, in Objective-C this implies that the API could return nil. Swift formalizes how optional values are treated.
 
+//: An optional value either contains a value or contains nil. Optional values work exactly the same if the optional is a struct instance or a class instance. To indicate that a value might be an optional write a question mark after the type to mark the value as optional.
+
+//: You can create an optional value with an initial value, nil or Optional.None. The compiler will treat the property differently if the property has not been assigned.
+var optionalString:String? = "a string"
+
+let optionalString2:String?
+println(optionalString2.dynamicType)
+// println("\(optionalString2)")
+
+let optionalString3:String? = nil
+println(optionalString3)
+println(optionalString3.dynamicType)
+
+let optionalString4:String? = .None
+println(optionalString3)
+println(optionalString3.dynamicType)
+
+//: You can unwrap the optional to access its value using the ```if let``` together like so:
+if let aString = optionalString {
+    println(aString)
+}
+
+//: After a optional value has been assigned a value you can set it back to nil.
+
+optionalString = nil
+
+if let aString = optionalString {
+    println(aString)
+}
+else {
+    println("optionalString is nil")
+}
+
+//: And then back again.
+
+optionalString = "A new string"
+
+//: Optional is an enum with an associated value with the .Some enum value or .None. So can assign
+
+optionalString = Optional.None
+
+//: You can abbreviate that to:
+
+optionalString = .None
+
+//: You can also assign using the Optional .Some value.
+
+optionalString = Optional.Some("An even new string")
+
+//: You wouldn't normally do this but it demonstrates what an optional really is.
+
+struct WellHello {
+    let name:String?
+    let greeting:String
+    
+    init(message:String = "Well Hello!", name:String? = .None) {
+        greeting = message
+        self.name = name
+    }
+    
+    func printMessage() {
+        let theName = name ?? "Dolly"
+        println("\(greeting) \(theName)")
+    }
+}
+
+let hello1:WellHello? = WellHello()
+
+//: You can unwrap an optional using the ? operator.
+
+let theGreeting = hello1?.greeting
+
+hello1?.printMessage()
+
+//: #### Did you notice in the printMessage function the use of ??
+
+//: This is called the nil coalescing operator. Effectively what it does is it checks to see if the value is nil, if it is nil it returns the value after the operator, otherwise it returns the unwrapped optional value.
+
+//: If you know that an optional value will be valid at a certain point then you can use the ! operator. This operator does a forced unwrap of the optional value and if the value is nil/Optional.None then it will cause a crash.
+var hello2:WellHello? = Optional.None
+// hello2!.printMessage()
+
+println("Do we get here? - we shouldn't if the above line is uncommented as it causes a crash")
+
+hello2 = WellHello(name: "Louie", message: "It is nice to see you back here where you belong")
+
+println("test")
+
+//: Even though the following force unwraps the name property because hello2 is optionally unwrapped theName remains an optional.
+let theName = hello2?.name!
+
+println(theName)
+println("theName type: \(theName.dynamicType)")
+
+//: You can force unwrap both.
+let theName2 = hello2!.name!
+println(theName2)
+println("theName2 type: \(theName2.dynamicType)")
+
+//: You can add more than optional unwrap in each if statement. See the printMessageFrom function.
+class GreetingsFrom {
+    let country:String
+    let greetingMessage:WellHello?
+    
+    init(country:String = "UK", greetingMessage:WellHello? = .None) {
+        self.country = country
+        self.greetingMessage = greetingMessage
+    }
+    
+    func printMessageFrom() {
+        if let theMessage = self.greetingMessage,
+            let theName = theMessage.name {
+            println("\(theName) from \(country) says: \(theMessage.greeting)")
+        }
+        else if let theMessage = self.greetingMessage {
+            println("\(country) says: \(theMessage.greeting)")
+        }
+        else {
+            println("Greetings from \(country)")
+        }
+    }
+}
+
+let greetingsFromUK:GreetingsFrom? = GreetingsFrom(greetingMessage: hello2)
+
+let greetingsFromUKName = greetingsFromUK?.greetingMessage?.name
+
+//: Because either of ? results can be operating on an optional without a value the result is also an optional but an optional of the type of name. String.
+
+println("Type of greetingsFromUKName: \(greetingsFromUKName.dynamicType)")
+
+greetingsFromUK!.printMessageFrom()
 
