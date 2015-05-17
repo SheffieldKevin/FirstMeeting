751b4ada4664e3f94af9fd9b97735ccf36e2dfbd
Kevin Meaney <ktam@yvs.eu.com>
2015-05-17 13:36:49 +0100

    Editing talk, preparing for giving it.

--- SheffieldSwift.playground/Contents.swift
+++ SheffieldSwift.playground/Contents.swift
@@ -1,13 +1,12 @@
-//: ## 1 @SheffieldSwift 19 May 2015
-//: ### Swift is an powerful new language with some interesting features.
+//: ![Sheffield Swift](Swift.png)
+//: ## @SheffieldSwift 19 May 2015
+//: ### Swift is a powerful new language with some interesting features.
 //: I'll be discussing a few of those today
+//: * Swift is strictly typed with type inference
 //: * Mutability versus immutability
 //: * Difference between Class, Struct and enum instances and why that is important
 //: * Optionals
-//: * Functions are first class objects in Swift
-//: * Enums and associative values
-//:   * Possibly also pattern matching on the enum and associated values
-//:   * The Result type
+//: * Enums
 
 //: ### Swift is strictly typed and does type inference
 var str = "Hello, playground"
@@ -20,7 +19,7 @@ str = "@SheffieldSwift says hello back"
 
 //: #### So what is going on here?
 //: Swift is stricly typed so the compiler gives every variable or constant a specific type, the type cannot be changed after the variable is declared. This is different to ruby where a variable can be reassigned to an object of any type. If a type isn't specified but the type can be inferred from the assigned value when the variable or constant is declared then the compiler gives the variable or constant a type. If the type cannot be inferred the compiler will emit an error message.
-//: So the type of str is determined by the compiler at the point of decleration at compile time and is determined to be a string.
+//: So the type of str is determined by the compiler at the point of decleration at compile time and is determined to be a string. I use the dynamicType property of a class or struct instance to inform us what the type is.
 println("str type is: \(str.dynamicType)")
 
 //: #### We can specify the type in the decleration
@@ -46,7 +45,6 @@ println(newString)
 
 //: A variable or a constant does not have to have an initial value as long as the property is assigned a value before it is accessed. In this case though the decleration requires the type to be specified. The compiler will not attempt to look ahead to infer the type from later information.
 
-// let anotherString
 let theString:String
 
 if str == "Hello, playground" {
@@ -57,7 +55,10 @@ else {
 }
 
 println(theString)
-//: ### Difference between Struct and Object instances.
+
+//: ##### I would recommend that you always prefer let over var.
+
+//: ### What is the difference between instances of Structs and Classes?
 
 class Person {
     var firstName = ""
@@ -69,7 +70,7 @@ class Person {
     }
     
     func printName() {
-        println("\(firstName)  \(lastName)")
+        println("\(firstName) \(lastName)")
     }
 }
 
@@ -117,7 +118,7 @@ myCity.printCity()
 //: The word 'instance' is a more general term used to refer to objects of classes or structs.
 
 //: ### Optionals - An important feature of Swift but initially painful
-//: If a property can't be assigned when declared then its value will be either undefined or nil. Some APIs return an Optional value, in Objective-C this implies that the API could return nil. Swift formalizes how optional values are treated.
+//: If a property can't be assigned when declared then its value will be either undefined or nil. Some APIs return an Optional value, in Objective-C this implies that the API could return nil. Swift formalizes how what are nil values in Objective-C are treated.
 
 //: An optional value either contains a value or contains nil. Optional values work exactly the same if the optional is a struct instance or a class instance. To indicate that a value might be an optional write a question mark after the type to mark the value as optional.
 
@@ -126,6 +127,8 @@ var optionalString:String? = "a string"
 
 let optionalString2:String?
 println(optionalString2.dynamicType)
+
+//: Optional.None is different to undefined. If you have not set a property prior to it being accessed it is not by default set to Optional.None. Instead the compiler reports an error.
 // println("\(optionalString2)")
 
 let optionalString3:String? = nil
@@ -136,12 +139,12 @@ let optionalString4:String? = .None
 println(optionalString3)
 println(optionalString3.dynamicType)
 
-//: You can unwrap the optional to access its value using the ```if let``` together like so:
+//: You can unwrap the optional to access its value using ```if let``` like so:
 if let aString = optionalString {
     println(aString)
 }
 
-//: After a optional value has been assigned a value you can set it back to nil.
+//: After an optional value has been assigned a value you can set it back to nil.
 
 optionalString = nil
 
@@ -156,7 +159,7 @@ else {
 
 optionalString = "A new string"
 
-//: Optional is an enum with an associated value with the .Some enum value or .None. So can assign
+//: Optional is an enum, the enum value .Some has an associated value whilst the enum value .None does not. The value associated with .Some is the value returned from the optional value when the optional is unwrapped. Assigning an optional property with nil or .None does the same thing.
 
 optionalString = Optional.None
 
@@ -205,8 +208,6 @@ println("Do we get here? - we shouldn't if the above line is uncommented as it c
 
 hello2 = WellHello(name: "Louie", message: "It is nice to see you back here where you belong")
 
-println("test")
-
 //: Even though the following force unwraps the name property because hello2 is optionally unwrapped theName remains an optional.
 let theName = hello2?.name!
 
@@ -218,7 +219,7 @@ let theName2 = hello2!.name!
 println(theName2)
 println("theName2 type: \(theName2.dynamicType)")
 
-//: You can add more than optional unwrap in each if statement. See the printMessageFrom function.
+//: You can add more than one optional unwrap in each if statement. See the printMessageFrom function.
 class GreetingsFrom {
     let country:String
     let greetingMessage:WellHello?
@@ -246,9 +247,18 @@ let greetingsFromUK:GreetingsFrom? = GreetingsFrom(greetingMessage: hello2)
 
 let greetingsFromUKName = greetingsFromUK?.greetingMessage?.name
 
-//: Because either of ? results can be operating on an optional without a value the result is also an optional but an optional of the type of name. String.
+//: Because either of ? results can be operating on an optional without a value the result is also an optional but an optional of the type of name: String.
 
 println("Type of greetingsFromUKName: \(greetingsFromUKName.dynamicType)")
 
 greetingsFromUK!.printMessageFrom()
+
+//: ## What next
+//: There are a few more fundamental Swift topics which I'd like to cover but this is enough for today.
+//: * Functions as first class objects in Swift
+//: * Enums and associative values
+//:   * Pattern matching on the enum and associated values (switch statements)
+//:   * The Result type
+
+//: ![Sheffield Swift](Swift.png)
 
